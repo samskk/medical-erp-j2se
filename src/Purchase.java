ef9bc90555ecb24177cdbb190faf3fb13e5545e1
saikrishnak <saikrishnak1997@gmail.com>
2021-07-17 22:24:21 +0530

    project settings

--- src/Purchase.java
+++ src/Purchase.java
@@ -8,12 +8,12 @@
  *
  * @author depthin
  */
-public class purchase extends javax.swing.JFrame {
+public class Purchase extends javax.swing.JFrame {
 
     /**
      * Creates new form purchase
      */
-    public purchase() {
+    public Purchase() {
         initComponents();
     }
 
@@ -544,20 +544,21 @@ public class purchase extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Purchase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new purchase().setVisible(true);
+                new Purchase().setVisible(true);
             }
         });
     }
--- src/SalesReturn.java
+++ src/SalesReturn.java
@@ -8,12 +8,12 @@
  *
  * @author depthin
  */
-public class salesreturn extends javax.swing.JFrame {
+public class SalesReturn extends javax.swing.JFrame {
 
     /**
      * Creates new form salesreturn
      */
-    public salesreturn() {
+    public SalesReturn() {
         initComponents();
     }
 
@@ -517,20 +517,21 @@ public class salesreturn extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(salesreturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SalesReturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(salesreturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SalesReturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(salesreturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SalesReturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(salesreturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SalesReturn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new salesreturn().setVisible(true);
+                new SalesReturn().setVisible(true);
             }
         });
     }
